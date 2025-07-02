021e031a3ee46cd09d9c679d02e2388869b2921e
Daan Bakker <daan.bakker@amsterdam.nl>
2025-07-02 12:58:45 +0200

    tweaks

--- send_email.py
+++ send_email.py
@@ -2,6 +2,7 @@ import os
 import smtplib
 from email.message import EmailMessage
 import pandas as pd
+from email.utils import formataddr
 
 csv_path = "prijzen_s25_ultra.csv"
 
@@ -12,10 +13,15 @@ try:
 except Exception as e:
     df_text = f"Kon CSV niet lezen: {e}"
 
+from_name = os.environ.get('FROM_NAME', 'PriceCheckApp')
+from_email = os.environ.get('FROM_EMAIL')
+to_name = os.environ.get('TO_NAME', '')
+to_email = os.environ.get('TO_EMAIL')
+
 msg = EmailMessage()
 msg['Subject'] = 'Dagelijkse Prijzen S25 Ultra'
-msg['From'] = os.environ.get('FROM_EMAIL')
-msg['To'] = os.environ.get('TO_EMAIL')
+msg['From'] = formataddr((from_name, from_email))
+msg['To'] = formataddr((to_name, to_email))
 msg.set_content(f"Hieronder de actuele prijzen:\n\n{df_text}")
 
 with open(csv_path, 'rb') as f:
